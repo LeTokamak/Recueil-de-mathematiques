5f377142f94577b54c0bcc02d912d67ef9d3627a
LeTokamak <camp.clem.camp@gmail.com>
2024-06-16 01:10:08 +0200

    table trigo v1

---
+++ Section 3 - Trigonometrie/v1 - Trigonometrie/Table Trigo/Calcul table trigo.py
@@ -0,0 +1,351 @@
+import excel as fExc
+
+import math as m
+from datetime import datetime, timedelta
+
+derNbPremier = 2000
+
+# %% Fonctions
+
+def tronque(n, nbDecimal):
+    """
+    Coupe un réel n avec nbDecimal chiffres après la virgule
+    """
+    return float(int(n*(10**nbDecimal)))/(10**nbDecimal)
+
+def tronqueEntier(x, nbChiffre):
+    """
+    Coupe un réel x avec nbChiffre chiffres significatifs
+    """    
+    return int(x/(10**nbChiffre))*(10**nbChiffre)
+   
+def FonctArrondie(n, fonct, fonctReciproque, nbDecimal):
+    """
+    Renvoie la valeur de la fonction fonct(n) arrondie à nbDecimal près
+    Cet arrondi est le plus proche possible de la valeur exacte
+    """
+    try :
+        yExact = fonct(n)
+        yMin = tronque(yExact, nbDecimal)
+        yMaj = yMin + 10**(-1 * nbDecimal)
+    
+        if fonctReciproque(yMin) - n < fonctReciproque(yMaj) - n :
+            return yMin
+        else :
+            return yMaj
+    except : 
+        return 404
+
+def MiseEnPage(nb, nbChiffre, nbDecimal, Espace = True, affichageSigne = False, retourneEntier = False):
+    """
+    Met en page un nombre en : 
+        - ajoutant des 0 devant le nombre pour que le nb de chiffres devant la virgule soit égal à nbChiffre
+        - ajoutant des 0 après la virgule pour que le nb de chiffres après la virgule soit égal à nbDecimal
+        - ajoutant des espaces de séparation entre les groupes de chiffres (Espace = True)
+        - ajoutant un signe devant le nombre (affichageSigne = True)
+        - retournant un nombre entier (retourneEntier = True)
+    """
+    if "e" in str(nb) : 
+        return str(nb)
+    
+    if nb == -1 : return "- 1"
+    if nb == 0  : return "0"
+    if nb == 1  : return "1"
+    
+    if nb == 404 or nb > 1_000_000_000 or nb < -1_000_000_000: return "⨉"
+    
+    
+    
+    x  = round(nb, nbDecimal)
+    nb = abs(nb)
+    
+    if Espace :
+        # Détermination du nombre de 0 à écrire devant le nombre
+        nbZeroAv = nbChiffre - len(str(int(nb)))
+        if nbZeroAv < 0 :
+            print(nb)
+            return "ERREUR ! nbZeroAv < 0"
+        
+        # Détermination de la taille du groupe de poids le plus fort (en se basant sur la convention d'écriture "12 054")
+        nbChiffreGroupeAv = nbChiffre % 3
+        if nbChiffreGroupeAv == 0 :
+            nbChiffreGroupeAv = 3 
+
+        # Écriture du nombre avec ses espaces de séparation
+        strNbInt = nbZeroAv * "0" + str(int(nb))
+        strNbFinal = strNbInt[:nbChiffreGroupeAv] 
+        if len(str(int(nb))) != nbChiffreGroupeAv :
+            strNbFinal += " "
+        
+        for i in range(len(strNbInt[nbChiffreGroupeAv:]) ):
+            strNbFinal += strNbInt[nbChiffreGroupeAv + i]
+            if (i+1) % 3 == 0 and i != len(strNbInt[nbChiffreGroupeAv:])-1:
+                strNbFinal += " "
+            
+        if not retourneEntier :
+            strNbFinal += "."
+           
+            decimales = str(nb)[len(str(int(nb)))+1:]
+            
+            for i in range (nbDecimal):
+                if i < len(decimales) :
+                    if i % 3 == 0 and i != 0:
+                        strNbFinal += " "
+                    strNbFinal += decimales[i]
+                else :
+                    strNbFinal += "0"
+    
+    else :
+        strNbFinal = nbZeroAv * "0" + str(nb) + nbDecimal * "0"
+        
+    if affichageSigne and x < 0:
+        strNbFinal = "- " + strNbFinal
+    
+    strNbFinal = strNbFinal.replace(" .", ".")
+    
+    return strNbFinal
+        
+def EstPremier(n):
+    """
+    Renvoie True si n est premier, False sinon
+    """
+    d = 2
+    while d*d <= n :
+        if n % d == 0 :
+            return False
+        d += 1
+    return True
+
+def ListeNbPremierJusqua(nMax):
+    """
+    Renvoie la liste des nombres premiers jusqu'à nMax
+    """
+    n = 2
+    L = []
+    
+    while n < nMax :
+        if EstPremier(n):
+            L.append(n)
+        n += 1
+    return L
+
+LPremier = ListeNbPremierJusqua(derNbPremier)
+
+def Simplification(n,d):
+    """
+    Simplifie une fraction n/d
+    Renvoie une chaîne de caractère, le numérateur et le dénominateur simplifiés, les facteurs premiers du numérateur et du dénominateur
+    """
+    if n == 0 : return "0",1,1,1,1 ;    
+    
+    nFact = []
+    nNbTest = LPremier[:].copy()
+    dFact = []
+    dNbTest = LPremier[:].copy()
+    
+    while len(nNbTest) != 0 : # Nominateur
+        for i in nNbTest:
+            if n % i == 0 :
+                nFact.append(i)
+                n = n / i
+            else :
+                nNbTest.remove(i)
+                
+    while len(dNbTest) != 0 : # Dénominateur
+        for i in dNbTest:
+            if d % i == 0 :
+                dFact.append(i)
+                d = d / i
+            else :
+                dNbTest.remove(i)
+                
+    for i in nFact :
+        if i in dFact:
+            nFact.remove(i)
+            dFact.remove(i)
+
+    for i in dFact :
+        if i in nFact:
+            nFact.remove(i)
+            dFact.remove(i)
+    
+    for i in nFact :
+        if i in dFact:
+            nFact.remove(i)
+            dFact.remove(i)
+
+    for i in dFact :
+        if i in nFact:
+            nFact.remove(i)
+            dFact.remove(i)
+    
+    nomi,deno = 1,1
+    
+    for i in nFact:
+        nomi = nomi * i
+
+    for i in dFact:
+        deno = deno * i
+    
+    if nomi == 1 :
+        strNomi = ""
+    else :
+        strNomi = str(nomi)
+        
+    if n < 0 :
+        signe = "- "
+    else :
+        signe = ""
+    
+    if nomi == deno :
+        strFract = signe + "π"
+    else : 
+        strFract = signe + strNomi + " π / " + str(deno)
+        
+    return strFract,n,d,nFact,dFact
+
+def exp10 (x):
+    return 10**x
+
+# %% Programme
+
+# nPrem = -1800
+# nDern = 1800
+
+nPrem = 0
+nDern = 1800
+
+baseLog = 10
+
+nomi = 1
+deno = 2
+
+nbChiffre = 1
+nbDecimal = 9
+
+COLONNE_DEGRE = 1
+COLONNE_RADIAN = 2
+COLONNE_FRACTION_1 = 3
+COLONNE_FRACTION_2 = 4
+COLONNE_FRACTION_3 = 5
+COLONNE_SIN = 6
+COLONNE_COS = 7
+COLONNE_TAN = 8
+COLONNE_LOG_SIN = 9
+COLONNE_LOG_COS = 10
+COLONNE_LOG_TAN = 11
+
+Intro = ["Degré", "Rad", None, None, None, "sin (x)", "cos (x)", "tan (x)", "log |sin (x)|", "log |cos (x)|", "log |tan (x)|"]
+
+wb = fExc.creation_fichier_excel()
+ws = wb.active
+
+# Premiere ligne
+for e in Intro :
+    cellule = ws.cell(row = 1, column = Intro.index(e) + 1)
+    cellule.value = e
+    cellule.alignment = fExc.alignement(alignement_horizontal="c", alignement_vertical="c")
+    cellule.fill = fExc.couleur_fond(fExc.fond_entete)
+    cellule.font = fExc.police(est_en_gras = True, couleur=fExc.blanc)
+
+ws.merge_cells(start_row=1, start_column=COLONNE_RADIAN, end_row=1, end_column=COLONNE_FRACTION_3)
+
+for a in range(nPrem, nDern):
+    
+    angleDeg = a/10
+    angleRad = angleDeg * m.pi/180
+    strFract,nomi,deno,x,y = Simplification(a, 1800)
+
+    numLigne = 2 + a % 50 + 51*(a // 50)
+    
+    # Angle en degré
+    cellule = ws.cell(row = numLigne, column = COLONNE_DEGRE)
+    if str(angleDeg)[-2:] == ".0":
+        cellule.value = "  "+str(angleDeg)[:-2]
+    else :
+        cellule.value = "  "+str(angleDeg)
+    cellule.alignment = fExc.alignement(alignement_horizontal="l", alignement_vertical="c")
+    cellule.fill = fExc.couleur_fond(fExc.fond_degre)
+    cellule.font = fExc.police(couleur=fExc.blanc, est_en_gras=True)
+    
+    # Angle en radian
+    cellule = ws.cell(row = numLigne, column = COLONNE_RADIAN)
+    cellule.value = MiseEnPage(angleRad,1,9, affichageSigne = True)
+    cellule.alignment = fExc.alignement(alignement_horizontal="c", alignement_vertical="c")
+    cellule.fill = fExc.couleur_fond(fExc.fond_rad)
+    cellule.border = fExc.bordure(style="dashed", couleur=fExc.blanc, a_droite=True)
+    
+    # Fraction
+    if "/" in strFract :
+        cellule = ws.cell(row = numLigne, column = COLONNE_FRACTION_1)
+        cellule.value = strFract.split(" / ")[0]
+        cellule.alignment = fExc.alignement(alignement_horizontal="r", alignement_vertical="c")
+        cellule.fill = fExc.couleur_fond(fExc.fond_rad)
+        
+        cellule = ws.cell(row = numLigne, column = COLONNE_FRACTION_2)
+        cellule.value = "/"
+        cellule.alignment = fExc.alignement(alignement_horizontal="c", alignement_vertical="c")
+        cellule.fill = fExc.couleur_fond(fExc.fond_rad)
+        
+        cellule = ws.cell(row = numLigne, column = COLONNE_FRACTION_3)
+        cellule.value = strFract.split(" / ")[1]
+        cellule.alignment = fExc.alignement(alignement_horizontal="l", alignement_vertical="c")
+        cellule.fill = fExc.couleur_fond(fExc.fond_rad)
+    else :
+        cellule = ws.cell(row = numLigne, column = COLONNE_FRACTION_1)
+        cellule.value = strFract
+        cellule.alignment = fExc.alignement(alignement_horizontal="c", alignement_vertical="c")
+        cellule.fill = fExc.couleur_fond(fExc.fond_rad)
+        
+        ws.merge_cells(start_row=numLigne, start_column=COLONNE_FRACTION_1, end_row=numLigne, end_column=COLONNE_FRACTION_3)
+        
+    # sin(x)
+    cellule = ws.cell(row = numLigne, column = COLONNE_SIN)
+    cellule.value = MiseEnPage( FonctArrondie(angleRad,m.sin,m.asin,12),nbChiffre, nbDecimal, affichageSigne = True)
+    cellule.alignment = fExc.alignement(alignement_horizontal="c", alignement_vertical="c")
+    cellule.fill = fExc.couleur_fond(fExc.fond_blanc)
+    
+    # cos(x)
+    cellule = ws.cell(row = numLigne, column = COLONNE_COS)
+    cellule.value = MiseEnPage( FonctArrondie(angleRad,m.cos,m.acos,12),nbChiffre, nbDecimal, affichageSigne = True)
+    cellule.alignment = fExc.alignement(alignement_horizontal="c", alignement_vertical="c")
+    cellule.fill = fExc.couleur_fond(fExc.fond_colonne)
+    
+    # tan(x)
+    cellule = ws.cell(row = numLigne, column = COLONNE_TAN)
+    cellule.value = MiseEnPage( FonctArrondie(angleRad,m.tan,m.atan,12),3, nbDecimal, affichageSigne = True)
+    cellule.alignment = fExc.alignement(alignement_horizontal="c", alignement_vertical="c")
+    cellule.fill = fExc.couleur_fond(fExc.fond_blanc)
+    cellule.border = fExc.bordure(style="double", couleur=fExc.noir, a_droite=True)
+    
+    # log |sin(x)|
+    cellule = ws.cell(row = numLigne, column = COLONNE_LOG_SIN)
+    cellule.value = MiseEnPage( FonctArrondie(abs(m.sin(angleRad)),m.log10,exp10,12),nbChiffre, nbDecimal, affichageSigne = True)
+    cellule.alignment = fExc.alignement(alignement_horizontal="c", alignement_vertical="c")
+    cellule.fill = fExc.couleur_fond(fExc.fond_blanc)
+    
+    # log |cos(x)|
+    cellule = ws.cell(row = numLigne, column = COLONNE_LOG_COS)
+    cellule.value = MiseEnPage( FonctArrondie(abs(m.cos(angleRad)),m.log10,exp10,12),nbChiffre, nbDecimal, affichageSigne = True)
+    cellule.alignment = fExc.alignement(alignement_horizontal="c", alignement_vertical="c")
+    cellule.fill = fExc.couleur_fond(fExc.fond_colonne)
+    
+    # log |tan(x)|
+    cellule = ws.cell(row = numLigne, column = COLONNE_LOG_TAN)
+    cellule.value = MiseEnPage( FonctArrondie(abs(m.tan(angleRad)),m.log10,exp10,12),nbChiffre, nbDecimal, affichageSigne = True)
+    cellule.alignment = fExc.alignement(alignement_horizontal="c", alignement_vertical="c")
+    cellule.fill = fExc.couleur_fond(fExc.fond_blanc)
+    
+    if (a + 1) % 50 == 0:
+        
+        for e in Intro :
+            cellule = ws.cell(row = numLigne + 1, column = Intro.index(e) + 1)
+            cellule.value = e
+            cellule.alignment = fExc.alignement(alignement_horizontal="c", alignement_vertical="c")
+            cellule.fill = fExc.couleur_fond(fExc.fond_entete)
+            cellule.font = fExc.police(est_en_gras = True, couleur=fExc.blanc)
+            
+        ws.merge_cells(start_row=numLigne + 1, start_column=COLONNE_RADIAN, end_row=numLigne + 1, end_column=COLONNE_FRACTION_3)
+
+fExc.redimensionnement_colonne_auto_optimal(ws)
+wb.save(f"Section 3 - Trigonometrie/v1 - Trigonometrie/Table Trigo/sample_{str(datetime.now()).replace(':', '_')}.xlsx")
---
+++ Section 3 - Trigonometrie/v1 - Trigonometrie/Table Trigo/excel.py
@@ -0,0 +1,197 @@
+import openpyxl as opxl
+import csv
+
+# %% Couleurs
+
+noir         = "00000000" #000000
+rouge        = "00FF0000" #FF0000
+bleu         = "000000CC" #0000CC
+vert_clair   = "00CCFF99" #CCFF99
+rouge_clair  = "00FF9999" #FF9999
+jaune_clair  = "00FFFF99" #FFFF99
+gris_clair   = "00C0C0C0" #C0C0C0
+blanc        = "00FFFFFF" #FFFFFF
+
+fond_entete  = "00222222" #222222
+fond_degre   = "00666666" #666666
+fond_rad     = "00AAAAAA" #AAAAAA
+fond_colonne = "00DDDDDD" #DDDDDD
+fond_blanc   = "00FFFFFF" #FFFFFF
+
+
+
+def ouvrir_fichier_excel(lien_fichier_excel) :
+    """
+    Cette fonction ne supporte que les formats suivant : 
+    .xlsx, .xlsm, .xltx, .xltm
+    """
+    wb = opxl.load_workbook(filename = lien_fichier_excel)
+    return wb
+
+def ouvrir_fichier_csv(lien_fichier_csv, separateur = ";") :
+    """
+    Cette fonction ne supporte que le format suivant : 
+    .csv
+    """
+    liste_lignes = []
+    
+    with open(lien_fichier_csv, newline='') as csvfile:
+        for row in csv.reader(csvfile, delimiter = separateur, quotechar='|'):
+            liste_lignes.append(row)
+            
+    return liste_lignes
+
+
+
+def creation_fichier_excel():
+    wb = opxl.Workbook()
+    return wb
+
+
+
+def police( taille          = 11, 
+            est_en_gras     = False, 
+            est_en_italique = False, 
+            est_souligne    = False, 
+            couleur         = noir,
+            police          = "Calibri"):
+    
+    if est_souligne : est_souligne = ("singleAccounting", "doubleAccounting", "double", "single")[-1]
+    else            : est_souligne =  "none"
+    
+    font = opxl.styles.Font( name      = police         ,
+                             size      = taille         ,
+                             bold      = est_en_gras    ,
+                             italic    = est_en_italique,
+                             underline = est_souligne   ,
+                             color     = couleur         )
+    
+    return font
+
+
+
+def couleur_fond(couleur = blanc):
+    
+    fond = opxl.styles.PatternFill(start_color = couleur,
+                                   end_color   = couleur,
+                                   fill_type   = 'solid')
+    
+    return fond
+
+
+
+def alignement(alignement_horizontal="c", alignement_vertical="c", angle_texte = 0):
+    
+    liste_horizontale = ("fill", "general", "justify", "center", "left", "right"  , "centerContinuous", "distributed" )
+    liste_verticale   = (                   "justify", "center", "top" , "bottom" ,                     "distributed" )
+    
+    if alignement_horizontal in ("g", "l", "gauche", "left"  ) : position_horizontale = liste_horizontale[4]
+    if alignement_vertical   in ("h", "t", "haut"  , "top"   ) : position_verticale   = liste_verticale  [2]
+    
+    if alignement_horizontal in ("c",      "centre", "center") : position_horizontale = liste_horizontale[3]
+    if alignement_vertical   in ("c",      "centre", "center") : position_verticale   = liste_verticale  [1]
+    
+    if alignement_horizontal in ("d", "r", "droite", "right" ) : position_horizontale = liste_horizontale[5]
+    if alignement_vertical   in ("b",      "bas"   , "bottom") : position_verticale   = liste_verticale  [3]
+    
+    return opxl.styles.Alignment(horizontal   = position_horizontale, 
+                                 vertical     = position_verticale  , 
+                                 textRotation = angle_texte          )
+
+
+def commentaire ( texte           = "",
+                  couleur_de_fond = jaune_clair, 
+                  auteur          = "par_defaut",
+                  largeur         = 700,
+                  hauteur         = 200  ):
+    
+    commentaire = opxl.comments.Comment(texte, author = auteur)
+    commentaire.width = largeur     # Largeur du commentaire en pixels
+    commentaire.height = hauteur    # Hauteur du commentaire en pixels
+    # commentaire.width = 200     # Largeur du commentaire en pixels
+    # commentaire.height = 100    # Hauteur du commentaire en pixels
+    # commentaire.visible = True  # Afficher le commentaire
+    # commentaire.anchor = 'E'    # Ancrer le commentaire à droite de la cellule
+    # commentaire.font = openpyxl.styles.Font(name='Arial', size=12, italic=True)
+    commentaire.fill = couleur_fond(couleur_de_fond)
+    
+    return commentaire
+
+def redimensionnement_colonne_auto_optimal(feuille):
+    
+    dims = {}
+    for row in feuille.rows :
+        for cell in row :
+            if cell.value : 
+                dims[cell.column] = max((dims.get(cell.column, 0), len(str(cell.value))))    
+    
+    for col, value in dims.items() :
+        print(opxl.utils.get_column_letter(col), end=" - ")
+        print(value+1)
+        feuille.column_dimensions[opxl.utils.get_column_letter(col)].width = value+1
+        #feuille.column_dimensions[opxl.utils.get_column_letter(col)].auto_size = True
+    """
+    for colonne in feuille.columns :
+        colonne.
+    """
+    
+def bordure( style = "thin", couleur = noir ,a_droite = False, a_gauche = False, en_haut = False, en_bas = False):
+    """
+    style = {
+        "dashDot", 
+        "mediumDashDotDot", 
+        "double", 
+        "mediumDashed", 
+        "dashDotDot", 
+        "medium",
+        "thick",
+        "dashed", 
+        "mediumDashDot", 
+        "dotted", 
+        "hair",
+        "slantDashDot",
+        "thin"
+    }
+    """
+    bordure = opxl.styles.Side(style=style, color=couleur)
+    
+    if a_droite : return opxl.styles.Border(right=bordure)
+    if a_gauche : return opxl.styles.Border(left=bordure)
+    if en_haut  : return opxl.styles.Border(top=bordure)
+    if en_bas   : return opxl.styles.Border(bottom=bordure)
+    
+    if a_droite and a_gauche : return opxl.styles.Border(right=bordure, left=bordure)
+    if a_droite and en_haut  : return opxl.styles.Border(right=bordure, top=bordure)
+    if a_droite and en_bas   : return opxl.styles.Border(right=bordure, bottom=bordure)
+    if a_gauche and en_haut  : return opxl.styles.Border(left=bordure, top=bordure)
+    if a_gauche and en_bas   : return opxl.styles.Border(left=bordure, bottom=bordure)
+    if en_haut  and en_bas   : return opxl.styles.Border(top=bordure, bottom=bordure)
+    
+    if a_droite and a_gauche and en_haut : return opxl.styles.Border(right=bordure, left=bordure, top=bordure)
+    if a_droite and a_gauche and en_bas  : return opxl.styles.Border(right=bordure, left=bordure, bottom=bordure)
+    if a_droite and en_haut  and en_bas  : return opxl.styles.Border(right=bordure, top=bordure, bottom=bordure)
+    if a_gauche and en_haut  and en_bas  : return opxl.styles.Border(left=bordure, top=bordure, bottom=bordure)
+    
+    if a_droite and a_gauche and en_haut and en_bas : return opxl.styles.Border(right=bordure, left=bordure, top=bordure, bottom=bordure)
+    
+
+
+"""
+
+
+# grab the active worksheet
+ws = wb.active
+
+# Data can be assigned directly to cells
+ws['A1'] = 42
+
+# Rows can also be appended
+ws.append([1, 2, 3])
+
+# Python types will automatically be converted
+import datetime
+ws['A2'] = datetime.datetime.now()
+
+# Save the file
+wb.save("sample.xlsx")
+"""
